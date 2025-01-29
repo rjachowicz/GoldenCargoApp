c1c339a364dba3deb69bedfd5d8bb6f6e50acefd
rjachowicz <robertjach74@gmail.com>
2025-01-29 14:25:25 +0100

    added menu

---
+++ app/src/main/java/com/goldencargo/app/IncidentsReportsActivity.java
@@ -0,0 +1,13 @@
+package com.goldencargo.app;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class IncidentsReportsActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_incidents_reports);
+    }
+}
